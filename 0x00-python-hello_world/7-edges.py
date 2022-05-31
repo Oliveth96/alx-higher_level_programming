8b37e6c45bcde0e711e2c5c29301aa7eed432011
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-05-31 17:14:47 +0100

    Complete this source code to print object-oriented programming with Python, followed by a new line.

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,6 +3,6 @@ word = "Holberton"
 word_first_3 = "Hol"
 word_last_2 = "on"
 middle_word = "olberto"
-print(f"First_3_letters: {word_first_3}")
-print(f"Last_2_letters: {word_last_2}")
-print(f"Middle_word: {middle_word}")
+print(f"First 3 letters:{word_first_3}")
+print(f"Last2 letters:{word_last_2}")
+print(f"Middle word:{middle_word}")
