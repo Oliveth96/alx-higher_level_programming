6d1993a22edecec4d163657b8c971f5e99a6ccb0
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-05 03:04:30 +0100

    Write a function that finds multiples of 2 in a list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if not my_list:
-        return my_list
-    a_list = list(my_list)
-    for index in a_list:
-        if index % 2 == 0:
-            a_list[index] = True
-        else:
-            a_list[index] = False
-    return a_list
+    if my_list:
+        return list(map(lambda n: not (n % 2), my_list))
+    return None
