ce7ff1147a98588cba9a689ca4508907a274016a
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-21 10:17:00 +0100

    Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    product = 0
+    for i in range(2, 5):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            else:
+                product += (a ** b) / i
+        except:
+            product = b + a
+            break
+    return product
