ad4e7930a9ef7e92d712d33d7193ed43b05350a7
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-21 20:19:30 +0100

    Writes a class Node that defines a node of a singly linked list by:

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -6,20 +6,21 @@
 class Node():
     """The definition of a singly-linked list node
     """
+
     def __init__(self, data, next_node=None):
-        """ Instantiate a node
+        """ Instantiates a node
         """
         self.data, self.next_node = data, next_node
 
     @property
     def data(self):
-        """ Get the data stored in a node
+        """" to retrieve it
         """
         return self.__data
 
     @data.setter
     def data(self, data):
-        """ Set the data stored in a node
+        """to set it
         """
         if not isinstance(data, int):
             raise TypeError("data must be an integer")
@@ -27,13 +28,13 @@ class Node():
 
     @property
     def next_node(self):
-        """ Get the next node
+        """" to retrieve it
         """
         return self.__next_node
 
     @next_node.setter
     def next_node(self, next_node):
-        """ Set the next node
+        """to set it
         """
         if next_node is not None and not isinstance(next_node, Node):
             raise TypeError("next_node must be a Node object")
@@ -41,10 +42,10 @@ class Node():
 
 
 class SinglyLinkedList():
-    """ Definition of a singly-linked list
+    """ The definition of a singly-linked list
     """
     def __init__(self):
-        """ Instantiate a singly-linked list
+        """ Instantiates a singly-linked list
         """
         self.__head = None
 
@@ -53,5 +54,5 @@ class SinglyLinkedList():
         """
 
     def sorted_insert(self, value):
-        """ Inset a Node into a list sorted in ascending order
+        """ Inserts a Node into a list sorted in ascending order
         """
