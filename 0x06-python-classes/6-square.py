23efb8499af02a4c6e9adbc223a5c462ae2ae0db
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-21 15:01:49 +0100

    Writes a class Square that defines a square by: (based on 5-square.py)

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+""" Module that defines a 'Square'
+"""
+
+
+class Square():
+    """An empty definition of a 'Square'
+    """
+    __size = True
+
+    def __init__(self, size=0, position=(0, 0)):
+        """ Instantiates a 'Square'
+        """
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """" to retrieve it
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """to set it
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def position(self):
+        """to retrieve it
+        """
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """to set it
+        """
+        if not (isinstance(position, tuple) and
+                len(position) == 2 and
+                isinstance(position[0], int) and
+                isinstance(position[1], int) and
+                position[0] >= 0 and
+                position[1] >= 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = position
+
+    def area(self):
+        """ returns the current 'Square' area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """that prints in stdout the 'Square' with the character #
+        """
+        if self.size:
+            print('\n' * self.position[1], end="")
+            print('\n'.join(
+                [' ' * self.position[0] + '#' * self.size] * self.size
+            ))
+        else:
+            print()
