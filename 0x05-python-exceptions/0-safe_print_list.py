8dba4fb3df97d986c999d667b449b05c07747421
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-21 09:28:26 +0100

    Writes a function that prints x elements of a list.

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-def safe_print_integer(my_list=[], x=0):
-    '''print at most x items from a list'''
-    try:
-        for i in range(x):
-            print(my_list[i], end='')
-        return x
-    except IndexError:
-        return i
-    finally:
-        print()
+def safe_print_list(my_list=[], x=0):
+    c = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            c += 1
+        except IndexError:
+            break
+    print()
+    return c
