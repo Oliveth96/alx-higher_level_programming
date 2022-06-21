964923a634ebb80f3152518ecc86dbd3f535fb6c
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-21 14:26:58 +0100

    Writes  a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module that defines a 'Square'
+"""
+
+
+class Square():
+    """An empty definition of a 'Square'
+    """
+    __size = True
+
+    def __init__(self, size=0):
+
+        """ Instantiates a 'Square'
+        """
+        self, __size = size
