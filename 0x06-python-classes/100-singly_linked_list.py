db797b95178da8bf2d4cfcd07d63db6c70fe2db7
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-21 20:28:43 +0100

    Writes a class Node that defines a node of a singly linked list by:

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -8,67 +8,73 @@ class Node():
     """
 
     def __init__(self, data, next_node=None):
-        """Initialization of Node Class"""
+        """ Instantiates a node
+        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Data"""
-        return self.__data
+        """" to retrieve it
+        """
+        return (self.__data)
 
     @data.setter
-    def data(self, DataValue):
-        """Set data"""
-        if type(DataValue) != int:
+    def data(self, data):
+        """to set it
+        """
+        if not isinstance(data, int):
             raise TypeError("data must be an integer")
-        self.__data = DataValue
+        self.__data = data
 
     @property
     def next_node(self):
-        """Node"""
-        return self.__next_node
+        """" to retrieve it
+        """
+        return (self.__next_node)
 
     @next_node.setter
-    def next_node(self, NodeValue):
-        """set Node"""
-        if NodeValue is not None and not isinstance(NodeValue, Node):
+    def next_node(self, value):
+        """to set it
+        """
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
-        self.__next_node = NodeValue
+        self.__next_node = value
 
 
 class SinglyLinkedList():
-    """Class SinglyLinkedList"""
+    """ The definition of a singly-linked list
+    """
+
     def __init__(self):
-        """Initialization of SinglyLinkedList"""
+        """ Instantiates a singly-linked list
+        """
         self.__head = None
 
-    def sorted_insert(self, DataValue):
-        """Inserts a nodes"""
-        NewNode = Node(DataValue)
+    def sorted_insert(self, value):
+        """ Inserts a Node into a list sorted in ascending order
+        """
+        new = Node(value)
         if self.__head is None:
-            self.__head = NewNode
-            return
-        if DataValue < self.__head.data:
-            NewNode.next_node = self.__head
-            self.__head = NewNode
-            return
-        actual = self.__head
-        while DataValue >= actual.data:
-            prev = actual
-            if actual.next_node:
-                actual = actual.next_node
-            else:
-                actual.next_node = NewNode
-                return
-        prev.next_node = NewNode
-        NewNode.next_node = actual
-
+            new.next_node = None
+            self.__head = new
+        elif self.__head.data > value:
+            new.next_node = self.__head
+            self.__head = new
+        else:
+            tmp = self.__head
+            while (tmp.next_node is not None and
+                    tmp.next_node.data < value):
+                tmp = tmp.next_node
+            new.next_node = tmp.next_node
+            tmp.next_node = new
+        
     def __str__(self):
-        """Class As a String"""
-        strg = ""
-        actual = self.__head
-        while actual:
-            strg += str(actual.data) + "\n"
-            actual = actual.next_node
-        return strg[:-1]
+        """ Generate a visual representation of a list
+        """
+        values = []
+        tmp = self.__head
+        while tmp is not None:
+            values.append(str(tmp.data))
+            tmp = tmp.next_node
+        return ('\n'.join(values))
