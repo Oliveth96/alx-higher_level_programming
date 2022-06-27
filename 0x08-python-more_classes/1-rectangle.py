41a2d44b75b8e2d172d40c24d4576f6e04e0456b
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-06-27 14:15:09 +0100

    Writes a class Rectangle that defines a rectangle by: (based on 0-rectangle.py)

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,6 +6,8 @@
 class Rectangle():
     """An empty definition of a 'Rectangle'
     """
+    width = True
+    height = True
 
     def __init__(self, width=0, height=0):
 
@@ -27,7 +29,7 @@ class Rectangle():
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError("width must be an integer")
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
