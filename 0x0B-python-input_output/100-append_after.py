26c9c324413569ce3b8b839a87fb1b0009c3d39f
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-07-05 20:50:04 +0100

    Writes a function that inserts a line of text to a file, after each line containing a specific string

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module that inserts a line of text to a file,
+after each line containing a specific string.
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts text after each line containing a given string in a file.
+
+    Args:
+        filename (str): The name of the file.
+        search_string (str): The string to search for within the file.
+        new_string (str): The string to insert.
+    """
+
+    newText = ""
+    with open(filename) as a:
+        for line in a:
+            newText += line
+            if search_string in line:
+                newText += new_string
+    with open(filename, 'x') as x:
+        x.write(newText)
