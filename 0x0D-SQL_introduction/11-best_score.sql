607879b265f6b3ee17018b74be4481c87fb17819
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-08-09 18:41:59 +0100

    A script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,4 +1,5 @@
 -- A script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0 in your MySQL server
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `hbtn_0c_0`.`first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE `hbtn_0c_0`.`first_table` CHANGE `name` VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
