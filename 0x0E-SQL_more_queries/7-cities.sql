10f8d1e4ddc3e98337bc7e43545f292e891f634e
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-08-10 22:19:37 +0100

    A script that creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,13 +1,14 @@
 -- A script that creates the database hbtn_0d_usa and the table cities 
 -- (in the database hbtn_0d_usa)
---To create the Database - hbtn_0d_usa
+
+-- To create the Database - hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 -- To create the table 'cities' in the database 'hbtn_0d_usa'
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT UNIQUE NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	state_id INT NOT NULL,
-	FOREIGN KEY(state_id) 
-	REFERENCES hbtn_0d_usa.states(id),
-	name VARCHAR(256) NOT NULL
-	);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    state_id INT NOT NULL,
+    FOREIGN KEY(state_id) 
+    REFERENCES hbtn_0d_usa.state(id)),
+    type = InnoDB;
