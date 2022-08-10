da6e38206f3a80f158aa3693705678285890eb1a
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-08-10 22:18:40 +0100

    A script that creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,5 @@
 -- A script that creates the database hbtn_0d_usa and the table cities 
 -- (in the database hbtn_0d_usa)
-
 --To create the Database - hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
