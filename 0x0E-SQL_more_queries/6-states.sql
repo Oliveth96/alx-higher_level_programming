ffe867e7751859624c31f4405789cd9f394b502c
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-08-10 21:44:00 +0100

    A script that creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,10 @@
+-- A script that creates the database hbtn_0d_usa and the table states
+-- (in the database hbtn_0d_usa)
+-- To create the Database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- To create the Table
+CREATE TABLE IF NOT EXISTS states (
+    id INT UNIQUE AUTO GENERATED NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
