73356b8bec3e3f052bdef69a94daaf43d5ea3e32
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-08-10 21:28:41 +0100

    A script that creates the database hbtn_0d_2 and the user user_0d_2

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,12 @@
+-- A script that creates the database hbtn_0d_2 and the user user_0d_2
+-- To create the DB
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+-- To create the User
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+-- To grant the User SELECT only provilege
+GRANT SELECT
+ON hbtn_0d_2
+TO 'user_0d_2'@'localhost';
