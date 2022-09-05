dbdd6a17105eb6d54706a508881f7bf4a90ca8cc
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-09-05 14:10:19 +0100

    Standard compliant script that prints a message depending of the number of arguments passed~

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 // A script that prints a message depending of the number of arguments passed
 
 if (process.argv.length === 0) {
-    console.log('No argument')
+    console.log('No argument');
 } else if (process.argv.length === 1) {
-    console.log('Argument found')
-} else console.log('Arguments found')
+    console.log('Argument found');
+} else console.log('Arguments found');
