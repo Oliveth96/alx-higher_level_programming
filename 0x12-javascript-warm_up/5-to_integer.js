82a74324d111ae7682504859c053b6744c6dfb5b
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-09-05 15:32:42 +0100

    Standard compliant script that prints the first argument that can be converted to an integer~

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 // A script that prints "" if the first argument can be converted to an intege
 
 if (isNaN(process.argv[2])) {
-    console.log('Not a number');
-  } else {
-    console.log('My number: ' + parseInt(process.argv[2]));
-  }
+  console.log("Not a number");
+} else {
+  console.log("My number: " + parseInt(process.argv[2]));
+}
