db931e86bc74c7fc4af15ce1f8f56c3042f4dbb4
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-09-05 14:06:24 +0100

    Standard compliant script that prints 'JavaScript is amazing'~

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A script that prints a message depending of the number of arguments passed
+
+if (process.argv.length === 0) {
+    console.log('No argument')
+} else if (process.argv.length === 1) {
+    console.log('Argument found')
+} else console.log('Arguments found')
