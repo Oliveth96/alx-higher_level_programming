e17e2cec1fca022d9ff7a7ef52f6df54283376b7
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-09-07 03:59:45 +0100

    An empty class Rectangle that defines a rectangle

---
+++ 0x13-javascript_objects_scopes_closures/0-main.js
@@ -0,0 +1,8 @@
+#!/usr/bin/mode
+// An empty class Rectangle that defines a rectangle
+
+const Rectangle = require('./0-rectangle');
+
+const r1 = new Rectangle();
+console.log(r1);
+console.log(r1.constructor);
--- 0x13-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,8 +1,4 @@
-#!/usr/bin/mode
-// An empty class Rectangle that defines a rectangle
+#!/usr/bin/node
+// Empty definition of a rectangle
 
-const Rectangle = require('./0-rectangle');
-
-const r1 = new Rectangle();
-console.log(r1);
-console.log(r1.constructor);
+module.exports = class Rectangle {};
