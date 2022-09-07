d741510e32283a7ecf1f3bda6260772502f8963c
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-09-07 04:07:38 +0100

    A class that takes in two arguments

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// A a class Rectangle that defines a rectangle
+// The constructor must take 2 arguments w and h
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+};
