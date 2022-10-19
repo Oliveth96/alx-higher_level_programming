240c2ea2ae8f5514984f929b0bb2f453a804a198
Oliveth96 <79146676+Oliveth96@users.noreply.github.com>
2022-10-19 08:44:32 +0100

    A script that reads and prints the content of a file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 //A script that reads and prints the contents of a file
 
-const rf = require('rf');
+const fs = require('fs');
 
-rf.readFile(process.argv[2], 'utf-8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
     if (err) {
         console.log(err);
     } else {
